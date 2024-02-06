209223b54a9af601491e63b1cdc848a4ee51b2fe
Aurélie <aurelie.dloughian@protonmail.com>
2024-02-06 11:11:56 +0100

    update css

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,4 +1,4 @@
-/* Toggler style switcher */
+/* ===================== Toggler style switcher ===================== */
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("clic", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
