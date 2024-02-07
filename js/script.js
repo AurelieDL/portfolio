c4fc1a150d954efee041155c0b0ebdbdbfdd2e17
Aurélie <aurelie.dloughian@protonmail.com>
2024-02-07 16:19:46 +0100

    scroll nav

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* =================== Typing animation =================== */
+var typed = new Typed(".typing", {
+    strings: ["", "développeuse web", "créative", "passionnée"],
+    typeSpeed: 100,
+    Backspeed: 100,
+    loop: true,
+    startDelay: 500,
+})
+
+/* =================== Scroll sections active link =================== */
+// get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+// add an event listner listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    //get current scroll position
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+
+        }
+    })
+
+}
