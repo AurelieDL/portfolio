fe736c987787eb38ff46f4ffc6eff8972ad46edf
Aurélie <aurelie.dloughian@protonmail.com>
2024-02-15 13:47:00 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -10,9 +10,10 @@ var typed = new Typed(".typing", {
 /* =================== Scroll sections active link =================== */
 // get all sections that have an id defined
 const sections = document.querySelectorAll("section[id]");
-
+// allSection = document.querySelectorAll(".section");
+// totalSection = allSection.length;
 // add an event listner listening for scroll
-window.addEventListener("scroll", navHighlighter);
+window.addEventListener("scroll", navHighlighter)
 
 function navHighlighter() {
     //get current scroll position
@@ -29,5 +30,36 @@ function navHighlighter() {
 
         }
     })
+}
 
+
+// const navTogglerBtn = document.querySelector(".nav-toggler"),
+//     sidebar = document.querySelector(".sidebar");
+// navTogglerBtn.addEventListener("click", () => {
+//     asideSectionTogglerBtn();
+// })
+
+// function asideSectionTogglerBtn() {
+//     sidebar.classList.toggle("open");
+//     navTogglerBtn.classList.toggle("open");
+
+// }
+
+/* SHOW SIDEBAR*/
+const navMenu = documentByID("sidebar"),
+    navToggle = documentByID("nav-toggler"),
+    navClose = documentByID("nav__close")
+/* SHOW SIDEBAR*/
+// Validate if constante exists
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+
+// Sidebar HIDDEN
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar')
+    })
 }
