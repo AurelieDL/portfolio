47ae8b6efbab46ff903e5b64c6a075ad442db676
Aurélie <aurelie.dloughian@protonmail.com>
2024-02-22 08:09:53 +0100

    good version, next=>toggler btn

--- js/script.js
+++ js/script.js
@@ -29,3 +29,49 @@ function navHighlighter() {
     })
 }
 
+
+
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length,
+    aside = document.querySelector(".sidebar");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+
+function asideSectionTogglerBtn() {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
+}
+
+
+/* =================== Test active section =================== */
+
+// const nav = document.querySelector(".nav"),
+//     navList = nav.nav.querySelectorAll("li"),
+//     totalNavList = navList.length,
+//     allSection = document.querySelectorAll(".section"),
+//     totalSection = allSection.length;
+// for (let i = 0; i < totalNavList; i++) {
+//     const a = navList[i].querySelector("a");
+//     a.addEventListener("click", function () {
+//         for (let j = 0; j < totalNavList; j++) {
+
+//             navList[j].querySelector("a").classList.remove("active");
+//         }
+//         this.classList.add("active")
+//         showSection(this);
+//     })
+// }
+
+// function showSection(element) {
+//     for (let i = 0; i < totalSection; i++) {
+//         allSection[i].classList.remove("active");
+//     }
+//     const target = element.length("href").split("#")[1];
+//     document.querySelector("#" = target).classList.add("active")
+// }
